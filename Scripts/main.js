6b7c42524a2051e0d6259d98779ea593f996159f
Timothy Miller <tim@webinspect.tech>
2020-09-29 12:15:00 +0000

    Add code to select inside any style of quotes

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,20 +1,36 @@
 
-exports.activate = function() {
-    // Do work when the extension is activated
-}
-
-exports.deactivate = function() {
-    // Clean up state before the extension is deactivated
-}
+exports.activate = function() {}
+exports.deactivate = function() {}
 
 
-// Invoked by the "Foobar" command
-nova.commands.register("more-selections.insertFoobar", (editor) => {
-    // Begin an edit session
-    var position = editor.selectedRange.start;
-    editor.edit(function(e) {
-        // Insert the string "Foobar"
-        e.insert(position, "Foobar");
-    });
+nova.commands.register("selectWithinQuotes", (editor) => {
+	let newSelectedRanges = [];
+	for (const range of editor.selectedRanges) {
+		const bounds = new Range(0, editor.document.length);
+		let head = range.start;
+		let tail = range.end;
+		
+		const isQuote = /"|'|`/;
+		
+		while (head > 0) {
+			head -= 1;
+			const char = editor.getTextInRange(new Range(head, head + 1));
+			
+			if (isQuote.test(char)) {
+				head += 1;
+				break;
+			}
+		}
+		
+		while (tail < bounds.end) {
+			const char = editor.getTextInRange(new Range(tail, tail + 1));
+			if (isQuote.test(char)) {
+				break;
+			}
+			tail += 1;
+		}
+		newSelectedRanges.push(new Range(head, tail));
+	}
+	editor.selectedRanges = newSelectedRanges;
 });
 
